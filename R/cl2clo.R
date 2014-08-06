7c7d4911784fcaa1ec86f63cce23e1d1ed2e66f6
Max Tuni <max@predictia.es>
2014-08-06 09:42:09 +0200

    renombrada variable tcc

--- R/cl2clo.R
+++ R/cl2clo.R
@@ -1,14 +1,14 @@
 #' @title Total cloud cover fraction to okta
 #' @description Conversion of total cloud cover in parts per unit to oktas (0-8)
 #' 
-#' @param tcc (1)
-#' @return vector of tcc in oktas
+#' @param cl (1)
+#' @return vector of cl in oktas
 #' @author M Tuni \email{max@@predictia.es}
 #' @keywords internal
 #' @family conversion
 #' @export
 
-tcc2tcco <- function(tcc) {
+cl2clo <- function(tcc) {
       tcco <- round(tcc * 8) 
       tcco[tcco < 0] = 0
       tcco[tcco > 8] = 8
